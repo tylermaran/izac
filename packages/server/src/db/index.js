a1e9f816ac0525d46e994b78adb9b6a6b86e3934
Jay Querie <jay@querie.cc>
2019-07-29 20:41:55 -0700

    move init database into an admin call

--- packages/server/src/db/index.js
+++ packages/server/src/db/index.js
@@ -4,20 +4,21 @@ const drink = require('./drink');
 
 module.exports = class Database {
   constructor(sqlite3_db_filepath) {
-    this.sqlite3_db = new sqlite3.Database(sqlite3_db_filepath);
+    const sqlite3_db = new sqlite3.Database(sqlite3_db_filepath);
+
+    // The cryptic code below creates closures and applies our sqlite3
+    // connection to the underlying database functions. This cleans up
+    // our External API so we don't have to pass along a sqlite3 conn
+    // for every call!
 
     this.bottle = {};
-    Object.keys(bottle).forEach((key) =>
-      this.bottle[key] = () => bottle[key].apply(
-        null,
-        [ this.sqlite3_db_filepath, ...arguments ]
-      ));
+    Object.keys(bottle).forEach((key) => this.bottle[key] = function () {
+      return bottle[key].apply(null, [ sqlite3_db, ...arguments ]);
+    });
 
     this.drink = {};
-    Object.keys(bottle).forEach((key) =>
-      this.drink[key] = () => drink[key].apply(
-        null,
-        [ this.sqlite3_db_filepath, ...arguments ]
-      ));
+    Object.keys(bottle).forEach((key) => this.drink[key] = function () {
+      return drink[key].apply(null, [ sqlite3_db, ...arguments ]);
+    });
   }
 }
--- packages/server/src/handlers/admin.js
+++ packages/server/src/handlers/admin.js
@@ -23,105 +23,93 @@ exports.pins.fire = (req, res) => {
 
 exports.database = {};
 
-exports.database.init = (sqlite3_db) => async (req, res) => {
-  await this.db.bottle.create(sqlite3_db);
-  await this.db.drink.create(sqlite3_db);
+exports.database.init = (db) => async (req, res) => {
+  await db.bottle.create();
+  await db.drink.create();
 
   // >>>> bottles (liquor)
-  await db.bottle.add(
-    sqlite3_db, "rum", 1.75, 'air', pins.bottle_rum); // id=1
-  await db.bottle.add(
-    sqlite3_db, "gin", 1.75, 'air', pins.bottle_gin); // id=2
-  await db.bottl.add(
-    sqlitee3_db, "vodka", 1.75, 'air', pins.bottle_vodka); // id=3
-  await db.bottle.add(
-    sqlite3_db, "scotch", 1.75, 'air', pins.bottle_scotch); // id=4
-  await db.bottle.add(
-    sqlite3_db, "irish whisky", 1.75, 'air', pins.bottle_irish_whisky); // id=5
-  await db.bottle.add(
-    sqlite3_db, "tequila", 1.75, 'air', pins.bottle_tequila); // id=6
-  await db.bottle.add(
-    sqlite3_db, "burbon", 1.75, 'air', pins.bottle_burbon); // id=7
+  await db.bottle.add("rum", 1.75, 'air', pins.bottle_rum); // id=1
+  await db.bottle.add("gin", 1.75, 'air', pins.bottle_gin); // id=2
+  await db.bottle.add("vodka", 1.75, 'air', pins.bottle_vodka); // id=3
+  await db.bottle.add("scotch", 1.75, 'air', pins.bottle_scotch); // id=4
+  await db.bottle.add("irish whisky", 1.75, 'air', pins.bottle_irish_whisky); // id=5
+  await db.bottle.add("tequila", 1.75, 'air', pins.bottle_tequila); // id=6
+  await db.bottle.add("burbon", 1.75, 'air', pins.bottle_burbon); // id=7
 
   // >>>> bottles (mixers)
-  await db.bottle.add(
-    sqlite3_db, "coke", 2, 'peristaltic',
-    pins.bottle_coke, pins.bottle_coke_reverse); // id=8
-  await db.bottle.add(
-    sqlite3_db, "ginger ale", 2, 'peristaltic',
-    pins.bottle_ginger_ale, pins.bottle_ginger_ale_reverse); // id=9
-  await db.bottle.add(
-    sqlite3_db, "tonic", 2, 'peristaltic',
-    pins.bottle_tonic, pins.bottle_tonic_reverse); // id=10
+  await db.bottle.add("coke", 2, 'peristaltic',
+                      pins.bottle_coke, pins.bottle_coke_reverse); // id=8
+  await db.bottle.add("ginger ale", 2, 'peristaltic',
+                      pins.bottle_ginger_ale, pins.bottle_ginger_ale_reverse); // id=9
+  await db.bottle.add("tonic", 2, 'peristaltic',
+                      pins.bottle_tonic, pins.bottle_tonic_reverse); // id=10
 
-  await db.bottle.add(
-    sqlite3_db, "lemon lime", 2, 'air', pins.bottle_lemon_lime); // id=11
-  await db.bottle.add(
-    sqlite3_db, "cranberry", 3.78541, 'air', pins.bottle_cranberry); // id=12
-
+  await db.bottle.add("lemon lime", 2, 'air', pins.bottle_lemon_lime); // id=11
+  await db.bottle.add("cranberry", 3.78541, 'air', pins.bottle_cranberry); // id=12
 
   const four_oz_in_liters = 0.118294;
   const shot_in_liters = 0.044;
   const one_shot_chaser = four_oz_in_liters - shot_in_liters;
 
   // >>>> drinks (RUM id=1)
-  await db.drink.add(sqlite3_db, "Rum (neat)", [
+  await db.drink.add("Rum (neat)", [
     { bottle_id: "1",  liters: shot_in_liters }
   ]);
 
-  await db.drink.add(sqlite3_db, "Rum Lemon-Lime", [
+  await db.drink.add("Rum Lemon-Lime", [
     { bottle_id: "1",  liters: shot_in_liters },
     { bottle_id: "12", liters: one_shot_chaser } //  - 0.044 =
   ]);
 
-  await db.drink.add(sqlite3_db, "Rum & Coke", [
+  await db.drink.add("Rum & Coke", [
     { bottle_id: "1",  liters: shot_in_liters },
     { bottle_id: "12", liters: one_shot_chaser }
   ]);
 
   // >>>> drinks (Gin id=2)
-  await db.drink.add(sqlite3_db, "Gin (neat)", [
+  await db.drink.add("Gin (neat)", [
     { bottle_id: "2",  liters: shot_in_liters }
   ]);
 
-  await db.drink.add(sqlite3_db, "Gin & Ginger Ale", [
+  await db.drink.add("Gin & Ginger Ale", [
     { bottle_id: "2",  liters: shot_in_liters },
     { bottle_id: "9",  liters: one_shot_chaser }
   ]);
 
   // >>>> drinks (VODKA id=3)
-  await db.drink.add(sqlite3_db, "Vodka (neat)", [
+  await db.drink.add("Vodka (neat)", [
     { bottle_id: "3",  liters: shot_in_liters }
   ]);
 
-  await db.drink.add(sqlite3_db, "Vodka Tonic", [
+  await db.drink.add("Vodka Tonic", [
     { bottle_id: "3", liters: shot_in_liters },
     { bottle_id: "10", liters: one_shot_chaser }
   ]);
 
-  await db.drink.add(sqlite3_db, "Vodka Cranberry", [
+  await db.drink.add("Vodka Cranberry", [
     { bottle_id: "3", liters: shot_in_liters },
     { bottle_id: "11", liters: one_shot_chaser }
   ]);
 
   // >>> drinks (SCOTCH id=4)
-  await db.drink.add(sqlite3_db, "Scotch (neat)", [
+  await db.drink.add("Scotch (neat)", [
     { bottle_id: "4",  liters: shot_in_liters }
   ]);
 
   // >>> drinks (Irish_Whisky id=5)
-  await db.drink.add(sqlite3_db, "Irish Whisky (neat)", [
+  await db.drink.add("Irish Whisky (neat)", [
     { bottle_id: "5",  liters: shot_in_liters }
   ]);
 
   // >>> drinks ("tequila" id=6)
-  await db.drink.add(sqlite3_db, "Tequila (neat)", [
+  await db.drink.add("Tequila (neat)", [
     { bottle_id: "6",  liters: shot_in_liters }
   ]);
 
   // >>> drinks ("burbon" id=7)
-  await db.drink.add(sqlite3_db, "Burbon (neat)", [
+  await db.drink.add("Burbon (neat)", [
     { bottle_id: "7",  liters: shot_in_liters }
   ]);
 
+  res.status(204).end();
 };
