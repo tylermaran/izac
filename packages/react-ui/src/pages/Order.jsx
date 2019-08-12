8dde614851a77287beb1c8ce8dae0d303ea49179
Jay Querie <jay@querie.cc>
2019-08-11 22:19:14 -0700

    minimal changes necessary to chat with the backend API ;)
    using hooks here to be cool --- sort of shoved this in and
    will need some more love later <3

--- packages/react-ui/src/pages/Order.jsx
+++ packages/react-ui/src/pages/Order.jsx
@@ -1,53 +1,68 @@
+//
+// @TODO: clean up console.log's / verbose comments in this file! (sorry)
+//
+
 // Importing Dependencies
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import API from 'barbot-api';
 
 // Importing styling
 import './Order.css'
 
+// Initialize our API to point to our backend's "base url"
+//
+// This "API" class can be looked at in the `barbot-api` package
+const api = new API('http://localhost:5000');
+
 const Order = (props) => {
 
-    let order = (drink) => {
-        console.log('Ordering a ' + drink);
-        fetch('http://localhost:5000/order/' + drink, {
-            method: 'POST',
-            mode: 'cors',
-            cache: 'no-cache',
-            // credentials: 'same-origin',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            redirect: 'follow',
-            referrer: 'no-referrer',
-            // body: JSON.stringify(data)
-        }).then((response) => {
-            return response.json();
-        }).then((data) => {
-            console.log(JSON.stringify(data));
-        });
-    }
+    // React Hooks Reference:
+    //
+    // https://reactjs.org/docs/hooks-reference.html#useeffect
+    //
+    const [drinks, setDrinks] = useState([]);
 
+    useEffect(() => {
+        api.drink.list().then(data => {
+            console.log(JSON.stringify(data, null, 4));
+            setDrinks(data.drinks)
+        });
+    }, []);
+
+    let order = async (drink) => {
+        // Print out what we are ordering
+        console.log('Ordering a ' + drink.name);
+
+        // Make the network request.
+        //
+        // THIS BLOCKS UNTIL FINISHED
+        const data = await api.drink.pour(drink.id);
+
+        // i.e. we don't print this out until the
+        // robot is done pouring the drink :)
+        console.log(JSON.stringify(data, null, 4));
+    };
 
     return (
         <div className='order'>
+
             <div className="header">
                 <h1>Bender's Bar</h1>
             </div>
+
             <div className="menu">
-                <h2>
-                    Order a Drink
-                </h2>
-                
-                <div className="drink" onClick={()=> order('Margarita')}>
-                    <div className="drink_name">Margarita</div>
-                </div>
-                <div className="drink" onClick={()=> order('Whiskey')}>
-                    <div className="drink_name">Whiskey</div>
-                </div>
-                
+
+                <h2>Order a Drink</h2>
+
+                <>{drinks.map(drink => (
+                    <div className="drink" onClick={() => order(drink)}>
+                        <div className="drink_name">{ drink.name }</div>
+                    </div>
+                ))}</>
 
             </div>
         </div>
     );
 }
-  
+
 export default Order;
