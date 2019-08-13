63daf3a8daddc2c9871c9cdba00ea87d1fd76ba2
tyler.maran <tyler.maran@gmail.com>
2019-08-13 11:48:32 -0700

    changed around the rpio with Jay. To be changed back

--- packages/web-server/src/robots.js
+++ packages/web-server/src/robots.js
@@ -1,13 +1,16 @@
-const rpio = require('rpio');
+const gpio = require('rpi-gpio')
+const gpiop = gpio.promise;
 
 exports.dispenseStraw = () => Promise.resolve(); // @TODO
 
 exports.on_then_off = (pin, ms) => new Promise((resolve, reject) => {
-  rpio.open(pin, rpio.OUTPUT, rpio.LOW);
-  rpio.write(pin, rpio.LOW);
+  return gpiop.setup(pin, gpio.DIR_OUT)
+    .then(() => {
+      return gpiop.write(pin, false)
+    }).then(() => {
+      return new Promise(r => setTimeout(r, ms));
+    }).then(() => {
+      return gpiop.write(pin, true);
+    });
 
-  setTimeout(() => {
-    rpio.write(pin, rpio.HIGH);
-    resolve();
-  }, ms);
 });
