db34e2b30197fc3a8f2f55b3cf9b546f72446627
tyler.maran <tyler.maran@gmail.com>
2019-08-16 11:10:59 -0700

    landing page has links

--- packages/react-ui/src/components/Header.jsx
+++ packages/react-ui/src/components/Header.jsx
@@ -17,19 +17,19 @@ const Header = (props) => {
                     <Link to='/settings'>Settings</Link>
                 </div>
 
-                    <div className="inner_tan">
-                        <div className="inner_red">
-                            <span className="rotate char1">i</span>
-                            <span className="rotate char2">Z</span>
-                            <span className="rotate char3">a</span>
-                            <span className="rotate char4">c</span>
-                            <span className="rotate char5">'</span>
-                            <span className="rotate char6">s</span>
-                            <span className="rotate char7">B</span>
-                            <span className="rotate char8">a</span>
-                            <span className="rotate char9">r</span>
-                        </div>
+                <div className="inner_tan">
+                    <div className="inner_red">
+                        <span className="rotate char1">i</span>
+                        <span className="rotate char2">Z</span>
+                        <span className="rotate char3">a</span>
+                        <span className="rotate char4">c</span>
+                        <span className="rotate char5">'</span>
+                        <span className="rotate char6">s</span>
+                        <span className="rotate char7">B</span>
+                        <span className="rotate char8">a</span>
+                        <span className="rotate char9">r</span>
                     </div>
+                </div>
      
             </div>
     )
--- packages/react-ui/src/pages/Landing.jsx
+++ packages/react-ui/src/pages/Landing.jsx
@@ -1,5 +1,6 @@
 // Importing Dependencies
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 // Importing styling
 import './Landing.css'
@@ -9,7 +10,22 @@ const Landing = (props) => {
 
     return (
         <div className="landing">
-            There's nothing here right now
+            <div className="inner_tan_landing">
+                <div className="inner_red_landing">
+                    <span className="rotate char1">i</span>
+                    <span className="rotate char2">Z</span>
+                    <span className="rotate char3">a</span>
+                    <span className="rotate char4">c</span>
+                    <span className="rotate char5">'</span>
+                    <span className="rotate char6">s</span>
+                    <span className="rotate char7">B</span>
+                    <span className="rotate char8">a</span>
+                    <span className="rotate char9">r</span>
+                </div>
+            </div>
+            <Link to='/order' className="enter">
+                Enter
+            </Link>
         </div>
     )
     
