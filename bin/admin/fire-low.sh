6d121a7de9410a0f8943ddbe020a7bbcc49b8f58
Jay Querie <jay@querie.cc>
2019-08-26 20:38:54 -0700

    create admin script to help with debugging board

---
+++ bin/admin/fire-low.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env dash
+
+if [ "$#" -ne 2 ]
+then
+  echo "fire-low.sh <pin-number> <duration-ms>"
+  exit 1
+fi
+
+PIN="${1}"
+DURATION_MS="${2}"
+WEB_SERVER_PORT=5000
+
+curl -XPOST "localhost:${WEB_SERVER_PORT}/admin/pins/${PIN}/fire" \
+     -H 'Content-Type: application/json' \
+     -d '{ "output": 0, "sleep_ms": 1000 }'
