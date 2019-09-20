c0e891a5d899063bca9d5faef21569d75c090ce2
Jay Querie <jay@querie.cc>
2019-09-19 22:29:27 -0700

    move configuration into an external module

--- packages/web-server/bin/start.js
+++ packages/web-server/bin/start.js
@@ -1,48 +1,8 @@
 #!/usr/bin/env node
 
-const path = require('path');
 const Server = require('../src/server');
 const migrations = require('../src/migrations');
-const cwd = process.cwd(); // store all files we create in the cwd.
-
-const config = {
-  port: process.env.WEB_SERVER_PORT || (() => {
-    console.error('you must set WEB_SERVER_PORT');
-    process.exit(1);
-  })(),
-  cors: {
-    origin: true,
-    credentials: true,
-    allowedHeaders: ['Content-Type']
-  },
-  client: {
-    react: {
-      baseDir: process.env.REACT_BUILD_DIR || (() => {
-        if (process.env.NODE_ENV === 'production') {
-          console.error('you must set REACT_BUILD_DIR');
-          process.exit(1);
-        }
-
-        return '/dev/null';
-      })()
-    }
-  },
-  server: {
-    pin: {
-      port: process.env.PIN_SERVER_PORT || (() => {
-        console.error('you must set PIN_SERVER_PORT');
-        process.exit(1);
-      })()
-    }
-  },
-  sqlite3: {
-    filename: path.join(cwd, 'db.sqlite3')
-  },
-  migrations: {
-    migrationsDirectory: path.join(__dirname, '..', 'migrations'),
-    stateStore: path.join(cwd, '.migrate')
-  }
-};
+const config = require('../config');
 
 (async function() {
   try {
