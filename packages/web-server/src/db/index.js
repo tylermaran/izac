51394f7b47e05efb85a50a354c01372f24e74409
Jay Querie <jay@querie.cc>
2019-09-19 19:13:25 -0700

    replace raw `sqlite3` connection with sequelize orm

--- packages/web-server/src/db/index.js
+++ packages/web-server/src/db/index.js
@@ -1,19 +1,23 @@
-const sqlite3 = require('sqlite3');
+const Sequelize = require('sequelize');
 
 module.exports = class Database {
   constructor(sqlite3_db_filepath) {
-    const sqlite3_db = new sqlite3.Database(sqlite3_db_filepath);
 
-    // The cryptic code below creates closures that apply our sqlite3
-    // connection to all database module functions. This cleans up
-    // our External API so we don't have to pass along a sqlite3 conn
-    // for every call!
+    const sequelize = new Sequelize({
+      dialect: 'sqlite',
+      storage: sqlite3_db_filepath
+    });
 
-    [
+    const dbModuleNames = [
       'device_type', 'device_action', 'device', 'pin',
       'bottle',
       'drink', 'drink_pour',
-    ].forEach(module_name => {
+    ];
+
+    // The cryptic code below creates closures that apply our sequelize
+    // instance to all db modules. This cleans up our External (public)
+    // API so we don't have to pass sequelize for every call.
+    dbModuleNames.forEach(module_name => {
       const mod = require(`./${module_name}`);
 
       Object.keys(mod).forEach((key) => {
@@ -22,7 +26,7 @@ module.exports = class Database {
         }
 
         this[module_name][key] = function () {
-          return mod[key].apply(null, [ sqlite3_db, ...arguments ]);
+          return mod[key].apply(null, [ sequelize, ...arguments ]);
         };
       });
     });
